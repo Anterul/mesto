33d1e8e617b79a82a6e9d356547917e6abe0720c
Andrey Shalaev <andre.shale@yandex.ru>
2022-08-03 20:34:10 +0300

    feat: полностью доделать вёрстку и логику попапа

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let page = document.querySelector('.page');
+let profileTitle = page.querySelector('.profile__title');
+let profileSubtitle = page.querySelector('.profile__subtitle');
+let editButton = page.querySelector('.profile__edit-button');
+let popup = page.querySelector('.popup');
+let closeButton = page.querySelector('.popup__close-button');
+let popupForm = page.querySelector('.popup__form');
+let nameInput = page.querySelector('.popup__name-input');
+let jobInput = page.querySelector('.popup__job-input');
+
+// функция, которая добавляет класс popup_opened если eго нет и убирает если он есть
+function popupToggle() {
+  popup.classList.toggle('popup_opened');
+}
+
+// при клике на кнопку с карандашом
+editButton.addEventListener('click', popupToggle);
+
+// при клике на иконку крестика удаляет класс popup_opened
+closeButton.addEventListener('click', popupToggle);
+
+// функция передаёт данные из строк value в заголовок и подзаголовок
+function saveButtonSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  popupToggle();
+}
+
+popupForm.addEventListener('submit', saveButtonSubmit);
