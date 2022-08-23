f489f656e1ec07b8701e39f00f6ef518443f5a94
Andrey Shalaev <andre.shale@yandex.ru>
2022-08-23 15:33:47 +0300

    feat: удалить карточки из html, добавить template, вызвать карточки из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
-let page = document.querySelector('.page');
-let profileTitle = page.querySelector('.profile__title');
-let profileSubtitle = page.querySelector('.profile__subtitle');
-let editButton = page.querySelector('.profile__edit-button');
-let popup = page.querySelector('.popup');
-let closeButton = page.querySelector('.popup__close-button');
-let popupForm = page.querySelector('.popup__form');
-let nameInput = page.querySelector('.popup__name-input');
-let jobInput = page.querySelector('.popup__job-input');
+const page = document.querySelector('.page');
+const profileTitle = page.querySelector('.profile__title');
+const profileSubtitle = page.querySelector('.profile__subtitle');
+const editButton = page.querySelector('.profile__edit-button');
+const cards = page.querySelector('.cards');
+const cardDeleteButton = page.querySelector('.card__delete-button');
+
+const popup = page.querySelector('.popup');
+const closeButton = page.querySelector('.popup__close-button');
+const popupForm = page.querySelector('.popup__form');
+const nameInput = page.querySelector('.popup__name-input');
+const jobInput = page.querySelector('.popup__job-input');
 
 // функция, которая добавляет класс popup_opened если eго нет и убирает если он есть
-function popupToggle() {
+function popupOnOff() {
   popup.classList.toggle('popup_opened');
 }
 
@@ -22,9 +25,65 @@ function saveButtonSubmit(evt) {
 }
 
 // при клике на кнопку с карандашом
-editButton.addEventListener('click', popupToggle);
+editButton.addEventListener('click', popupOnOff);
 
 // при клике на иконку крестика удаляет класс popup_opened
-closeButton.addEventListener('click', popupToggle);
+closeButton.addEventListener('click', popupOnOff);
 
 popupForm.addEventListener('submit', saveButtonSubmit);
+
+//массив из задания - убрать карточки из cards
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/* перебор массива, на каждой итерации передача из !!!!элемента массива в клонированную
+из template карточку и добавление изменённой карточки в cards */
+initialCards.forEach(item => {
+  const template = page.querySelector('.template').content;
+  const cardTemplate = template.querySelector('.card').cloneNode(true);
+  
+  cardTemplate.querySelector('.card__title').textContent = item.name;
+  cardTemplate.querySelector('.card__image').src = item.link;
+  cardTemplate.querySelector('.card__image').alt = item.name;
+
+  cards.append(cardTemplate);
+});
+
+
+
+console.log(cardLikeButton);
+
+/*
+addButton.addEventListener('click', () => {
+
+  ardTemplate.querySelector('.card__title').textContent = item.name;
+  cardTemplate.querySelector('.card__image').src = item.link;
+  cardTemplate.querySelector('.card__image').alt = item.name;
+
+  //initialCards.push({name: , link: })
+});
+*/
