1133838c755fe4fca840c2fc3bba1619df116078
Andrey Shalaev <andre.shale@yandex.ru>
2022-08-27 20:42:50 +0300

    fix: переместить массив, изменить функции - сделать универсальными

--- scripts/index.js
+++ scripts/index.js
@@ -1,154 +1,133 @@
-//  весь body.page
+// весь body.page
 const page = document.querySelector('.page');
-//  блок profile
+// блок profile
 const profileTitle = page.querySelector('.profile__title');
 const profileSubtitle = page.querySelector('.profile__subtitle');
-const editButton = page.querySelector('.profile__edit-button');
-const addButton = page.querySelector('.profile__add-button');
-//  блок cards
+const profileEditButton = page.querySelector('.profile__edit-button');
+const profileAddButton = page.querySelector('.profile__add-button');
+// блок cards
 const cards = page.querySelector('.cards');
-//  блок popup_name_profile
+// блок popup_name_profile
 const popupProfile = page.querySelector('.popup_name_profile');
 const popupProfileCloseButton = popupProfile.querySelector('.popup__close-button');
 const popupProfileForm = popupProfile.querySelector('.popup__form');
 const yourNameInput = popupProfile.querySelector('.popup__input_name_your-name');
 const jobInput = popupProfile.querySelector('.popup__input_name_job');
-//  блок popup_name_add-card
+// блок popup_name_add-card
 const popupAddCard = page.querySelector('.popup_name_add-card');
 const popupAddCardCloseButton = popupAddCard.querySelector('.popup__close-button');
 const popupAddCardForm = popupAddCard.querySelector('.popup__form')
 const cardNameInput = popupAddCard.querySelector('.popup__input_name_card-name');
 const cardLinkInput = popupAddCard.querySelector('.popup__input_name_card-link');
-//  блок popup_name_picture
+// блок popup_name_picture
 const popupPicture = page.querySelector('.popup_name_picture');
 const popupPictureCloseButton = popupPicture.querySelector('.popup__close-button');
-//  контент блока template
+const popupPictureImage = popupPicture.querySelector('.popup__image');
+const popupPictureTitle = popupPicture.querySelector('.popup__title_type_picture');
+// контент блока template
 const templateElement = page.querySelector('.template').content;
 
-//  ------------------------------- массив из задания и его перебор --------------------------------
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-//  перебор массива, на каждой итерации передача из объекта массива в клонированную
-//  из template карточку и добавление изменённой карточки в cards 
-initialCards.forEach((item) => {
-  const cardTemplate = templateElement.querySelector('.card').cloneNode(true);
-  cardTemplate.querySelector('.card__title').textContent = item.name;
-  cardTemplate.querySelector('.card__image').src = item.link;
-  cardTemplate.querySelector('.card__image').alt = item.name;
-  cardTemplate.querySelector('.card__image').addEventListener('click', openPopupPicture);
-  cardTemplate.querySelector('.card__like-button').addEventListener('click', likeCard);
-  cardTemplate.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-  cards.append(cardTemplate); // в cards записывается card на каждой итерации перебора
-});
-
-//  ----------------------------------------  функции  ---------------------------------------------
-
-//  функция, которая добавляет класс popup_opened если eго нет и убирает если он есть для первого popup
-function onOffPopupProfile() {
-  popupProfile.classList.toggle('popup_opened');
+// функция, которая добавляет класс popup_opened аргументу(попапу)
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-//  функция, которая передаёт строки из input.value в заголовок и подзаголовок блока profile
+// функция, которая удаляет класс popup_opened у аргумента
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// функция, которая добавляет/удаляет класс активного лайка
+function likeCard(element) {
+  element.classList.toggle('card__like-button_active');
+}
+
+// функция, которая удаляет переданный аргумент
+function deleteCard(element) {
+  element.remove();
+}
+
+// функция, которая передаёт строки из input.value в заголовок и подзаголовок блока profile
 function transmissionFromProfileInputs(evt) {
   evt.preventDefault();
   profileTitle.textContent = yourNameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  onOffPopupProfile();
+  closePopup(popupProfile);
 }
 
-//  функция, которая добавляет класс popup_opened если eго нет и убирает если он есть для второго popup
-function onOffPopupAddCard() {
-  popupAddCard.classList.toggle('popup_opened');
+// функция, которая сбрасывает инпуты
+function resetInputs(toFirstInput, toSecondInput, fromOne = '', fromTwo = '') {
+  toFirstInput.value = fromOne
+  toSecondInput.value = fromTwo
 }
 
-//  функция, которая объявляет в переменную ближайшую от клика карточку и удаляет её
-function deleteCard(evt) {
-  const closestDeleteButton = evt.target.closest('.card');
-  closestDeleteButton.remove();
+// функция, которая передаёт сылку и название картинки клонированной карточки в третий попап
+function openPopupPicture(templateLink, templateName) {
+  openPopup(popupPicture);
+  popupPictureImage.src = templateLink
+  popupPictureImage.alt = templateName
+  popupPictureTitle.textContent = templateName
 }
 
-//  функция, которая объявляет в переменную ближайшую от клика кнопку лайка и добавляет/удаляет класс
-function likeCard(evt) {
-  const closestLikeButton = evt.target.closest('.card__like-button');
-  closestLikeButton.classList.toggle('card__like-button_active');
+/* 
+функция, которая:
+клонирует карточку из template 
+аргументы функции передают ссылку и название в src, alt и заголовок карточки
+навешивает слушатели с функциями открытия третьего попапа и удаления и лайка самой карточки
+возвращает карточку
+*/
+function createCard(cardLink, cardName) {
+  const cardTemplate = templateElement.querySelector('.card').cloneNode(true);
+  const cardTemplateImage = cardTemplate.querySelector('.card__image');
+  const cardTemplateTitle = cardTemplate.querySelector('.card__title');
+  const cardTemplateDeleteButton = cardTemplate.querySelector('.card__delete-button');
+  const cardTemplateLikeButton = cardTemplate.querySelector('.card__like-button');
+
+  cardTemplateImage.src = cardLink;
+  cardTemplateImage.alt = cardName;
+  cardTemplateTitle.textContent = cardName;
+
+  cardTemplateImage.addEventListener('click', () => {
+    openPopupPicture(cardTemplateImage.src, cardTemplateTitle.textContent);
+  });
+
+  cardTemplateDeleteButton.addEventListener('click', () => {
+    deleteCard(cardTemplate);
+  });
+
+  cardTemplateLikeButton.addEventListener('click', () => {
+    likeCard(cardTemplateLikeButton);
+  });
+
+  return cardTemplate;
 }
 
-//  функция, которая добавляет класс popup_opened если eго нет и убирает если он есть для третьего popup
-function onOffPopupPicture() {
-  popupPicture.classList.toggle('popup_opened');
-}
+// перебор массива
+initialCards.forEach((item) => {
+  cards.append(createCard(item.link, item.name));
+});
 
-//  функция, которая клонирует карточку из template, передаёт ей строки из инпутов второго попапа, 
-//  навешивает слушатели с функциями удаления и лайка
+// функция, которая передаёт строки из input.value второго попапа в функцию создания карточки
 function addCard(evt) {
   evt.preventDefault();
-  const cardTemplate = templateElement.querySelector('.card').cloneNode(true);
-  cardTemplate.querySelector('.card__title').textContent = cardNameInput.value;
-  cardTemplate.querySelector('.card__image').src = cardLinkInput.value;
-  cardTemplate.querySelector('.card__image').alt = cardNameInput.value;
-  cardTemplate.querySelector('.card__image').addEventListener('click', openPopupPicture);
-  cardTemplate.querySelector('.card__like-button').addEventListener('click', likeCard);
-  cardTemplate.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-  cards.prepend(cardTemplate); // в cards записывается клонированная и измененная card 
-  cardNameInput.value = "";
-  cardLinkInput.value = "";
-  onOffPopupAddCard();
+  cards.prepend(createCard(cardLinkInput.value, cardNameInput.value));
+  resetInputs(cardNameInput, cardLinkInput);
+  closePopup(popupAddCard);
 }
 
-// функция, которая открывает третий попап и передаёт в него crs и alt выбранной карточки
-function openPopupPicture(evt) {
-  onOffPopupPicture();
-  popupPicture.querySelector('.popup__image').src = evt.target.closest('.card__image').src;
-  popupPicture.querySelector('.popup__image').alt = evt.target.closest('.card__image').alt;
-  popupPicture.querySelector('.popup__title_type_picture').textContent = evt.target.closest('.card__image').alt;
-}
-
-
-// -------------------------------------  слушатели  -----------------------------------------------
-
-//  слушатель, при клике по кнопке edit(карандаш) вызывает функцию onOffPopupProfile
-editButton.addEventListener('click', onOffPopupProfile);
-
-//  слушатель, при клике на closeButton(иконка крестика) вызывает функцию onOffPopupProfile
-popupProfileCloseButton.addEventListener('click', onOffPopupProfile);
-
-//  слушатель, при клике по кнопке в попапе "Сохранить" вызывает функцию transmissionFromProfileInputs
+// первый попап: открыть, сабмит, закрыть
+profileEditButton.addEventListener('click', () => { openPopup(popupProfile) });
 popupProfileForm.addEventListener('submit', transmissionFromProfileInputs);
+popupProfileCloseButton.addEventListener('click', () => {
+  closePopup(popupProfile);
+  resetInputs(yourNameInput, jobInput, profileTitle.textContent, profileSubtitle.textContent);
+});
 
-//  слушатель, при клике по кнопке add(плюс) вызывает функцию onOffPopupAddCard
-addButton.addEventListener('click', onOffPopupAddCard);
-
-//  слушатель, при клике на closeButton(иконка крестика) вызывает функцию onOffPopupAddCard
-popupAddCardCloseButton.addEventListener('click', onOffPopupAddCard);
-
-//  слушатель, при клике по кнопке в попапе "Создать" вызывает функцию addCard
+profileAddButton.addEventListener('click', () => { openPopup(popupAddCard) });
 popupAddCardForm.addEventListener('submit', addCard);
+popupAddCardCloseButton.addEventListener('click', () => {
+  closePopup(popupAddCard);
+  resetInputs(cardNameInput, cardLinkInput);
+});
 
-// слушатель, при клике по кнопке closeButton(иконка крестика) вызывает функцию onOffPopupPicture
-popupPictureCloseButton.addEventListener('click', onOffPopupPicture);
+popupPictureCloseButton.addEventListener('click', () => { closePopup(popupPicture) });
